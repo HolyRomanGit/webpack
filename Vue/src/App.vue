64c9bd190d36872832bd95ffcd15670a14b96d9d
HolyRomanGit <3487607472@qq.com>
2019-08-26 16:02:51 +0800

    VUE组件

---
+++ Vue/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  el: "#app",
+  data: {
+    
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style lang="stylus">
+
+
+</style>
